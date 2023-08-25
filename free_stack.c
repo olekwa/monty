239e0c5be71ea000fd24a1bd3d2aa86a78f188e5
olekwa <johnolekwa@gmail.com>
2023-08-24 22:11:58 -0700

    updated

---
+++ free_stack.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+* free_stack: This frees a double linked list
+* @head: The head of the stack
+*/
+void free_stack(stack_t *head)
+{
+	stack_t *aux;
+
+	aux = head;
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+	}
+}
---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - This will prints the top
+ * @head: This is head stack
+ * @counter: line_number
+ * Return: no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
